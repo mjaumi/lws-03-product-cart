d3f793f3d1b9032879691da76223a5e550475f02
mjaumi <mjaumi2864@gmail.com>
2023-02-28 17:29:10 +0600

    anchor tags changed to button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,16 +10,16 @@ const Navbar = ({ setRerouteToPage }) => {
     return (
         <nav className='bg-[#171C2A] py-4'>
             <div className='navBar'>
-                <a onClick={() => setRerouteToPage('home')} href='#home'>
+                <button onClick={() => setRerouteToPage('home')}>
                     <img src={logo} alt='LWS' className='max-w-[140px]' />
-                </a>
+                </button>
 
                 <div className='flex gap-4'>
-                    <a onClick={() => setRerouteToPage('home')} href='#home' className='navHome' id='lws-home'> Home </a>
-                    <a onClick={() => setRerouteToPage('cart')} href='#cart' className='navCart' id='lws-cart'>
+                    <button onClick={() => setRerouteToPage('home')} className='navHome' id='lws-home'> Home </button>
+                    <button onClick={() => setRerouteToPage('cart')} className='navCart' id='lws-cart'>
                         <i className='text-xl fa-sharp fa-solid fa-bag-shopping'></i>
                         <span id='lws-totalCart'>{totalCartItems}</span>
-                    </a>
+                    </button>
                 </div>
             </div>
         </nav>
